8117cb34a54972fdab0a39f88f87ad6770927a8c
Jason A. Storey <jason.storey@unrulymedia.com>
2015-11-26 13:41:18 +0000

    Add mouse controller to handle click on container and play single note

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+let Soundfont = require('soundfont-player'),
+    mouseController = require('./lib/mouse-controller');
+
+function init(config) {
+    let ctx = new AudioContext();
+    let soundfont = new Soundfont(ctx);
+    let instrument = soundfont.instrument('orchestral_harp');
+
+    let mouse = mouseController.create(config.container).init(); 
+
+    instrument.onready(() => {
+        mouse
+            .getEmitter()
+            .on('note-on', note => instrument.play(note, 0, -1));
+    });
+}
+
 module.exports = {
-    init: () => console.log('hello')
+    init: init
 };
 
